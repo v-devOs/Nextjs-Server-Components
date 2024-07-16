06d3f1d18b3fdaf93f8a61f2c815261fb5f17f84
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-16 15:51:55 -0600

    Realizando peticion a PokeApi

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,18 @@
 
-export default function PokemonsPage() {
+const getPokemons = async ( limit = 20, offset = 0 ) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then( res => res.json() );
+
+
+    return data;
+}
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+
   return (
     <div>
-      <h1>Pokemons Page</h1>
+      { JSON.stringify(pokemons)}
     </div>
   );
 }
