3d175ff3a1e40662029b1a8cb2d9d584341bfd2c
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-17 00:21:30 -0600

    Opteniendo informacion del pokemon

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,10 +1,22 @@
+import { Pokemon } from "@/pokemons";
 
 
 interface Props{
   params: { id: string }
 }
 
-export default function PokemonPage( {params}: Props) {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon: Pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: 'force-cache' // TODO: Cambiar esto en un futuro
+  })
+  .then( resp => resp.json())
+
+  return pokemon;
+}
+
+export default async function PokemonPage( {params}: Props) {
+
+  const pokemon = await getPokemon(params.id)
 
   return (
     <div>
