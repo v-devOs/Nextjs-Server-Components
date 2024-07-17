77578e2c920f7a4610beafd8d8659bcd9d04fa3a
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-16 22:21:30 -0600

    Mostrando pokemons de PokeApi

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -29,7 +29,7 @@ export const Sidebar = () => {
     <div
     style={{ width: '400px'}}
       id="menu"
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64  left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0  overflow-y-scroll"
     >
       <div id="logo" className="my-4 px-6">
         <h1 className="flex items-centertext-lg md:text-2xl font-bold text-white">
--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,18 +1,40 @@
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
 
-const getPokemons = async ( limit = 20, offset = 0 ) => {
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+const getPokemons = async ( limit = 20, offset = 0 ): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     .then( res => res.json() );
 
+  const pokemons = data.results.map( pokemon => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name
+  }))
 
-    return data;
+  return pokemons;
 }
 
 export default async function PokemonsPage() {
-  const pokemons = await getPokemons();
+  const pokemons = await getPokemons(151);
 
   return (
-    <div>
-      { JSON.stringify(pokemons)}
+    <div className="flex flex-col">
+
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+
+        {
+          pokemons.map( ({id, name}) => (
+            <Image
+            key={id}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+              width={100}
+              height={100}
+              alt={name}
+            />
+          ))
+        }
+
+      </div>
+      
     </div>
   );
 }
