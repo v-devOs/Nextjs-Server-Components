84e772436f5007fc02b95891067db817dfd91e7e
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-20 17:52:38 -0600

    Expotando hooks de react-redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
+import { useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
@@ -9,6 +10,10 @@ export const store = configureStore({
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
