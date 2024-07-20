7ceb16f91d4f36ff17e4971e3438b6691a475ab0
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-20 17:47:51 -0600

    Preparando el counter slice

---
+++ src/store/counter/counterSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface CounterState {
+  count: number
+}
+
+const initialState: CounterState = {
+  count: 5
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+
+    
+  }
+});
+
+export const {} = counterSlice.actions;
+
+export default counterSlice.reducer;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counter/counterSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counterReducer,
+
+  },
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
