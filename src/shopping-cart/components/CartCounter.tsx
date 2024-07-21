83c24ae66d564baf15de969c5b30b95ea775e590
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-21 17:30:59 -0600

    Server al client state

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,14 +1,21 @@
 'use client';
 import { useAppDispatch, useAppSelector } from "@/store";
-import { addOne, substractOne } from "@/store/counter/counterSlice";
+import { addOne, initCounterState, substractOne } from "@/store/counter/counterSlice";
+import { useEffect } from "react";
 
-export const CartCounter = () => {
+interface Props{
+  value?: number
+}
 
+export const CartCounter = ( {value = 0}: Props) => {
 
   const count = useAppSelector( state => state.counter.count );
   const dispatch = useAppDispatch();
 
-
+  useEffect(() => {
+    dispatch( initCounterState(value) )
+  }, [dispatch, value])
+  
   return (
     <>
       <span className="text-9xl"> { count } </span>
--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,17 +1,25 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CounterState {
-  count: number
+  count: number,
+  isReady: boolean
 }
 
 const initialState: CounterState = {
-  count: 5
+  count: 5,
+  isReady: false
 }
 
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
+    initCounterState( state, action: PayloadAction<number>){
+      if (state.isReady) return
+
+      state.count = action.payload
+      state.isReady = true
+    },
 
     addOne(state){
       state.count++;
@@ -31,6 +39,6 @@ const counterSlice = createSlice({
   }
 });
 
-export const { addOne, substractOne, resetCount } = counterSlice.actions;
+export const { addOne, substractOne, resetCount, initCounterState } = counterSlice.actions;
 
 export default counterSlice.reducer;
