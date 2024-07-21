6948cbbfcc1718c74c260551eaf0addcf7426dc4
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-21 17:55:47 -0600

    Solucion a la tarea

--- src/components/dashboard/SimpleWidget.tsx
+++ src/components/dashboard/SimpleWidget.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { IoCafeOutline } from "react-icons/io5"
 
 interface Props{
@@ -9,30 +10,40 @@ interface Props{
 }
 
 
-export const SimpleWidget = ({}: Props) => {
+export const SimpleWidget = ({ title, subtitle, label, icon, href }: Props) => {
   return (
     <div className="bg-white shadow-xl p-3 sm:min-w-[25%] min-w-full  rounded-2xl border-1 border-gray-50 mx-2 my-2">
       <div className="flex flex-col">
-        <div>
-          <h2 className="font-bold text-gray-600 text-center">Contador</h2>
-        </div>
+
+        {
+          label && 
+            <div>
+              <h2 className="font-bold text-gray-600 text-center">{ label }</h2>
+            </div>
+        }
         <div className="my-3">
           <div className="flex flex-row items-center justify-center space-x-1 ">
-            <div id="icon">
-              {/* Icono irá aquí */}
-              <IoCafeOutline size={50} className="text-blue-500"/>
-            </div>
+            {
+              icon
+            }
             <div id="temp" className="text-center">
-              <h4 className="text-4xl">Titulo</h4>
-              <p className="text-xs text-gray-500">Subtitulo</p>
+              <h4 className="text-4xl">{ title }</h4>
+
+              { subtitle && 
+                <p className="text-xs text-gray-500">{ subtitle }</p>
+              }
             </div>
           </div>
         </div>
 
-        <div className="w-full place-items-end text-right border-t-2 border-gray-100 mt-2">
-            <a href="#" className="text-indigo-600 text-xs font-medium">Más</a>
-        </div>
-        
+        { href &&
+          <div className="w-full place-items-end text-right border-t-2 border-gray-100 mt-2">
+            <Link 
+              href={ href }
+              className="text-indigo-600 text-xs font-medium"
+            >Mas</Link>
+          </div>
+        }
       </div>
     </div>
   )
--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -1,15 +1,23 @@
-'use client'
+'use client';
+import { useAppSelector } from '@/store';
 import { SimpleWidget } from './SimpleWidget'
+import { IoCartOutline } from 'react-icons/io5';
 
 export const WidgetsGrid = () => {
+
+  const count = useAppSelector( state => state.counter.count)
+
   return (
     <div className="flex flex-wrap p-2 ">
 
         <SimpleWidget
+          title={`${count}`}
+          href='/dashboard/counter'
+          icon={ <IoCartOutline size={70} className="text-blue-500"/>}
+          label='Contador'
+          subtitle='Productos agregados'
         
         />
-        {/* <SimpleWidget/> */}
-
       </div>
   )
 }
