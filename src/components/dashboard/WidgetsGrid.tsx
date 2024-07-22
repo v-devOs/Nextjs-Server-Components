dfd94e56b2ec09f573bc4cd1130ea5b2013d4e11
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-21 18:05:31 -0600

    RESTFUL Api

--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -10,14 +10,14 @@ export const WidgetsGrid = () => {
   return (
     <div className="flex flex-wrap p-2 ">
 
-        <SimpleWidget
-          title={`${count}`}
-          href='/dashboard/counter'
-          icon={ <IoCartOutline size={70} className="text-blue-500"/>}
-          label='Contador'
-          subtitle='Productos agregados'
-        
-        />
-      </div>
+      <SimpleWidget
+        title={`${count}`}
+        href='/dashboard/counter'
+        icon={ <IoCartOutline size={70} className="text-blue-500"/>}
+        label='Contador'
+        subtitle='Productos agregados'
+      
+      />
+    </div>
   )
 }
