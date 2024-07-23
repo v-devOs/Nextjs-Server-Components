7ad5f4515f70ee2d9d7eba3e195bbabdd230f948
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-22 18:07:12 -0600

    Mostrar cambios en el UI - Favoritos

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -1,7 +1,9 @@
+'use client'
 import Link from "next/link";
 import { SimplePokemon } from "../interfaces/simple-pokemon";
 import Image from "next/image";
-import { IoHeartOutline } from "react-icons/io5";
+import { IoHeart, IoHeartOutline } from "react-icons/io5";
+import { useAppSelector } from "@/store";
 
 interface Props{
   pokemon: SimplePokemon
@@ -10,6 +12,10 @@ interface Props{
 export const PokemonCard = ({pokemon}: Props) => {
 
   const { id, name } = pokemon
+
+  const isFavorite = useAppSelector( state => !!state.pokemons[id]);
+
+  console.log(isFavorite)
   return (
     <div className="mx-auto right-0 mt-2 w-60">
       <div className="flex flex-col  bg-white rounded overflow-hidden shadow-lg">
@@ -36,13 +42,17 @@ export const PokemonCard = ({pokemon}: Props) => {
         <div className="border-b">
           <Link href="/dashboard/main" className="px-4 py-2 hover:bg-gray-100 flex  items-center">
               <div className="text-red-600">
-                <IoHeartOutline/>
+                {
+                  isFavorite 
+                    ? <IoHeart/> 
+                    : <IoHeartOutline/>
+                }
               </div>
               <div className="pl-3">
                 <p className="text-sm font-medium text-gray-800 leading-none">
-                  No es favorito
+                  { isFavorite ? 'Es favorito' : 'No es favorito'}
                 </p>
-                <p className="text-xs text-gray-500">View your campaigns</p>
+                <p className="text-xs text-gray-500">Click para cambiar</p>
               </div>
           </Link>
         </div>
